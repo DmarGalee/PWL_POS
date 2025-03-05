05bc81e62a5c60f0fc3dce2730a79a5d3a088342
Damar Galih Fitrianto <damargalih1502@gmail.com>
2025-03-05 09:26:48 +0700

    Praktikum2.1_W4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Update data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama_lengkap' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
 
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username','nama_lengkap'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
